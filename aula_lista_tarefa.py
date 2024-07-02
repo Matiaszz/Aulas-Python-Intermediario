0dd6de3257b752eae3b0e981011036c7e7b3c468
Allan <alangiovanepaes@gmail.com>
2024-07-02 14:57:54 -0300

    Lista de tarefas com saves

---
+++ aula_lista_tarefa.py
@@ -0,0 +1,65 @@
+import os
+
+tasks = []
+pops = []
+commands = 'listar', 'desfazer', 'refazer', 'limpar', 'sair'
+FILE_SAVE = 'C:\\Users\\User\\Desktop\\AulasPython\\'
+FILE_SAVE += 'Lista_de_tarefas.txt'
+
+
+def undo():
+    try:
+        item = tasks.pop()
+        pops.append(item)
+    except IndexError:
+        print()
+        print('Nada para desfazer.')
+        print()
+        print()
+
+
+def remake():
+    if pops == []:
+        print('Nada a refazer.')
+    else:
+        last_index = pops.pop()
+        tasks.append(last_index)
+
+
+while True:
+    print('Comandos')
+    print('LISTAR', 'DESFAZER', 'REFAZER', 'SAIR')
+    value = input(
+        'Digite um comando ou uma tarefa para adicionar: ').lower()
+
+    if value not in commands:
+        os.system('cls')
+        tasks.append(value)
+
+    elif value == 'desfazer':
+        os.system('cls')
+        undo()
+
+    elif value == 'refazer':
+        os.system('cls')
+        remake()
+
+    elif value == 'listar':
+        os.system('cls')
+        print()
+        print('-' * 5, 'ITENS NA LISTA', '-' * 5)
+
+        with open('C:\\Users\\user\\desktop\\AulasPython\\lista_de_tarefas.txt', 'w+', encoding='utf-8') as file:
+            print('Itens que só podem ser alterados manualmente -🔹:')
+            print(file.read())
+
+        for item_task in tasks:
+            print(item_task)
+        print('-' * 30)
+        print()
+    elif value == 'sair':
+        print('-- Lista de Tarefas Salva - Programa Encerrado --')
+        with open('C:\\Users\\user\\desktop\\AulasPython\\lista_de_tarefas.txt', 'a', encoding='utf-8') as file:
+            file.write(
+                '\n'.join(['🔹' + task + '\n' for task in tasks]) + '----------SAVE----------' + '\n')
+        break
---
+++ aula_param_mut.py
@@ -0,0 +1,20 @@
+def add_clients(item, list=None):
+    if list == None:
+        list = []
+    list.append(item)
+    return list
+
+
+l1 = []
+c1 = add_clients('Allan', l1)
+c1.append('João')
+
+c2 = add_clients('Maria')
+c2.append('Matheus')
+
+c3 = add_clients('Jose')
+c3.append('Fernando')
+
+print(c1)
+print(c2)
+print(c3)
