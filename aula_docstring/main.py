35903bc0b0fb440fc259a5c72089afd106567dc1
Allan <alangiovanepaes@gmail.com>
2024-07-19 18:39:00 -0300

    Arquivos das aulas de documentação

---
+++ aula_docstring/main.py
@@ -0,0 +1,9 @@
+# import uma_linha
+# import varias_linhas
+# import docfuncao
+import docclass
+# print(dir(uma_linha))  # <-- mostra o que tem dentro da importação
+
+# Dá uma ajuda para quem é dev para saber o que é o módulo etc
+
+help(docclass)
---
+++ aula_docstring/uma_linha.py
@@ -0,0 +1,7 @@
+"""Uma breve descrição do módulo"""  # <-- isso é uma docstring
+
+variavel = 1
+
+
+def funcao():
+    return 0
